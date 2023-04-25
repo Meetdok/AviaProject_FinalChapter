cec81be9eb55f92afd7cb3e0136dfe9b49a55c0a
Artemi_grou@mail.ru <Artemi_grou@mail.ru>
2023-04-25 20:21:41 +1000

    Добавьте файлы проекта.

---
+++ WpfProject/ViewModels/EditServiceVM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WebProject.WebModels;
+using WpfProject.Tools;
+
+namespace WpfProject.ViewModels
+{
+    public class EditServiceVM : BaseTools
+    {
+        public CommandVM Save { get; set; }
+
+        public string Name { get; set; }
+        public int Cost { get; set; }
+
+        public EditServiceVM(Service service)
+        {
+            Save = new CommandVM(async () =>
+            {
+                var json = await HttpApi.Post("Services", "put", new Service
+                {
+                    ServiceId = service.ServiceId,
+                    ServiceType = Name,
+                    ServiceCost = Cost
+                });
+
+                MessageBox.Show("Сохранилось!");
+            });
+        }
+    }
+}
---
+++ WpfProject/ViewModels/ListServicesVM.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WebProject.WebModels;
+using WpfProject.Tools;
+using WpfProject.Windows;
+
+namespace WpfProject.ViewModels
+{
+    public class ListServicesVM : BaseTools
+    {
+        public Service SelectedItem { get; set; }
+        public Service service { get; set; }
+        private List<Service> services;
+        public string Name { get; set; }
+        public int Cost { get; set; }
+
+
+        public List<Service> Service
+        {
+            get => services;
+            set
+            {
+                services = value;
+
+                Signal();
+            }
+        }
+
+        public CommandVM EditService { get; set; }
+        public CommandVM DeleteService { get; set; }
+        public CommandVM Save { get; set; }
+
+        public ListServicesVM()
+        {
+            Task.Run(async () =>
+            {               
+                var json2 = await HttpApi.Post("Services", "ListServices", null);
+                Service = HttpApi.Deserialize<List<Service>>(json2);
+            });
+
+
+            EditService = new CommandVM(async () =>
+            {
+                service = SelectedItem;
+                new EditService(service).Show();
+            });
+
+            Save = new CommandVM(async () =>
+            {
+                var json = await HttpApi.Post("Services", "save", new Service
+                {
+                    ServiceType = Name,
+                    ServiceCost = Cost
+                });
+                Service result = HttpApi.Deserialize<Service>(json);
+
+                MessageBox.Show("Сохранилось!");
+            });
+
+            DeleteService = new CommandVM(async () =>
+            {
+                var json = await HttpApi.Post("Services", "delete", SelectedItem.ServiceId);
+
+                Task.Run(async () =>
+                {
+                    var json2 = await HttpApi.Post("Services", "ListServices", null);
+                    Service = HttpApi.Deserialize<List<Service>>(json2);
+                });
+            });
+        }
+    }
+}
